f4e5e024f6141f07157df8f08d2eef26f20dc169
NeerajKumar1001 <kumarn74858@gmail.com>
2025-05-08 19:45:56 +0530

    bug fix10

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,52 +1,37 @@
 import express from "express";
-import dotenv from "dotenv";
+import dontenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import cors from "cors"
+
 import path from "path";
 
-import { connectDB } from "./lib/db.js";
+import { connectDB } from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js"
 
-dotenv.config();
+dontenv.config();
+// const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://chatrix-fullstack-chat-app.onrender.com"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
+    origin: "http://localhost:5173",
+    credentials: true,
 }));
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
-// Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  // Handle any other routes - SPA fallback
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
-//   });
-}
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+   
+};
 
 server.listen(PORT, () => {
-  console.log("Server is running on PORT: " + PORT);
-  connectDB();
+    console.log("server is running on PORT:" + PORT)
+    connectDB();
 });
